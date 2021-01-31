94843a7c175c772278664bc8cd01732d3e7803b9
chernenko <artem.chernenko1496@gmail.com>
2021-01-31 17:25:15 +0200

    add connect to db and auth

--- app/src/Controller/TestController.php
+++ app/src/Controller/TestController.php
@@ -11,8 +11,19 @@ class TestController extends AbstractController
     #[Route('/test', name: 'test')]
     public function index(): Response
     {
+        $em = $this->getDoctrine()->getManager();
+        $em->getConnection()->connect();
+        $connected = $em->getConnection()->isConnected();
+        dd($connected);
+        $users = [
+            'Timmy',
+            'Sasha',
+            'Andrey',
+        ];
+
         return $this->render('test/index.html.twig', [
             'controller_name' => 'TestController',
+            'users' => $users,
         ]);
     }
 }
